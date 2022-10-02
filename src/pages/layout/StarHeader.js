588aebd2b8e412238b8d3a0712d729c6b1be1c99
beyzaabaas <beyza.baas@gmail.com>
2022-10-02 18:01:35 +0300

    header updated

--- src/pages/layout/StarHeader.js
+++ src/pages/layout/StarHeader.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assests/Star_Wars_Logo.png";
 
 function StarHeader() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div style={{ alignItems: "center" }}>
-        <img src={Logo} width={300} style={{ opacity: "0.8" }}></img>
+        <img
+          src={Logo}
+          width={300}
+          style={{ opacity: "0.8" }}
+          onClick={() => navigate(-1)}
+        ></img>
       </div>
     </>
   );
